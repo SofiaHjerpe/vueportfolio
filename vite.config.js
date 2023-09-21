d409374e43771572e45ff8f1543c75f6c58da16e
SofiaHjerpe <hjerpesofia@gmail.com>
2023-09-21 11:43:43 +0200

    c

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vueportfolio/',
+  base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
